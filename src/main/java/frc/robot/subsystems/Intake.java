4024855705d4c1a9c1c990c091e44d7df5d80bc4
BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>
2024-07-27 10:08:40 -0700

    Add intake🦟™️ (#2)
    * Create Intake.java
    
    * coded intake🦟™️
    
    * imported TalonFX and reorganized code
    
    * import mapIntake and neutralout
    
    * renamed setintakerollerspeed to setrolleroutput
    
    ---------
    
    Co-authored-by: Alena <112772895+Alenguye582@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotMap.mapIntake;
+
+import com.ctre.phoenix6.controls.NeutralOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Intake extends SubsystemBase {
+  TalonFX rollerMotor;
+
+  /** Creates a new Intake. */
+  public Intake() {
+    rollerMotor = new TalonFX(mapIntake.ROLLER_CAN, "rio");
+  }
+
+  public void setRollerOutput(double speed) {
+    rollerMotor.set(speed);
+  }
+
+  /**
+   * Sets the rollers to neutral.
+   */
+  public void setRollerNeutralOutput() {
+    rollerMotor.setControl(new NeutralOut());
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+
+  }
+
+}
