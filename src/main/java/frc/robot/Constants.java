290e5c0c62fb3e8b5910d78c589171cce2e9621e
Alena <112772895+Alenguye582@users.noreply.github.com>
2024-08-03 14:20:54 -0700

    updated swoffsets for offbot
    it workie :D

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -40,10 +40,10 @@ public final class Constants {
     // In Rotations: Obtain by aligning all of the wheels in the correct direction
     // and
     // copy-pasting the Raw Absolute Encoder value
-    public static final double FRONT_LEFT_ABS_ENCODER_OFFSET = -0.152832;
-    public static final double FRONT_RIGHT_ABS_ENCODER_OFFSET = 0.032471;
-    public static final double BACK_LEFT_ABS_ENCODER_OFFSET = -0.107666;
-    public static final double BACK_RIGHT_ABS_ENCODER_OFFSET = 0.095215;
+    public static final double FRONT_LEFT_ABS_ENCODER_OFFSET = -0.079834;
+    public static final double FRONT_RIGHT_ABS_ENCODER_OFFSET = 0.249268;
+    public static final double BACK_LEFT_ABS_ENCODER_OFFSET = -0.240479;
+    public static final double BACK_RIGHT_ABS_ENCODER_OFFSET = 0.210449;
 
     public static final InvertedValue DRIVE_MOTOR_INVERT = InvertedValue.CounterClockwise_Positive;
     public static final InvertedValue STEER_MOTOR_INVERT = InvertedValue.Clockwise_Positive;
