4c685ae3cd6f43934cfc64a02f2c11bab8b251dd
Alice <90939494+ACat701@users.noreply.github.com>
2024-08-31 12:59:38 -0700

    WPILib 2024.3.2 (#25)

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -12,7 +12,6 @@ import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.Velocity;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.constField;
@@ -86,7 +85,6 @@ public class Drive extends Command {
     } else if (chain.getAsBoolean()) {
       rVelocity = subDrivetrain.getVelocityToChain();
     }
-    SmartDashboard.putNumber("desired x velocity", xVelocity.in(Units.MetersPerSecond));
     subDrivetrain.drive(new Translation2d(xVelocity.in(Units.MetersPerSecond), yVelocity.in(Units.MetersPerSecond)),
         rVelocity.in(Units.RadiansPerSecond), isOpenLoop);
   }
