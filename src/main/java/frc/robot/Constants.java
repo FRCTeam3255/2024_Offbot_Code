a1ea23cb36842b457dc7cbb6c6db69cb754950df
ACat701 <90939494+ACat701@users.noreply.github.com>
2024-10-25 22:18:51 -0700

    "Disable the epilepsy" - T-Dawg 2024

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,7 @@ import java.util.function.Supplier;
 import com.ctre.phoenix.led.ColorFlowAnimation;
 import com.ctre.phoenix.led.FireAnimation;
 import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
 import com.ctre.phoenix.led.TwinkleAnimation;
 import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
 import com.ctre.phoenix.led.TwinkleAnimation.TwinklePercent;
@@ -646,6 +647,7 @@ public final class Constants {
   public static class constLEDs {
     public static final double LED_BRIGHTNESS = 1;
     public static final int LED_NUMBER = 192;
+    public static final int LED_STRIP_START_INDEX = 8;
 
     public static final int[] CLEAR_LEDS = { 0, 0, 0 };
     public static final int[] INTAKING_COLOR = { 0, 0, 0 };
@@ -653,8 +655,13 @@ public final class Constants {
     public static final int[] PREP_SUB_BACKWARDS_COLOR = { 255, 255, 0 };
     public static final int[] PREP_SPEAKER_COLOR = { 255, 130, 0 };
 
-    public static final ColorFlowAnimation STORE_FEEDER_COLOR = new ColorFlowAnimation(0, 255, 0, 0, 1, LED_NUMBER,
-        Direction.Forward);
+    // public static final ColorFlowAnimation STORE_FEEDER_COLOR = new
+    // ColorFlowAnimation(0, 255, 0, 0, 1, LED_NUMBER,
+    // Direction.Forward);
+
+    public static final StrobeAnimation STORE_FEEDER_COLOR = new StrobeAnimation(0, 255, 255,
+        0, 0.05, LED_NUMBER, LED_STRIP_START_INDEX);
+
     public static final RainbowAnimation READY_TO_SHOOT_COLOR = new RainbowAnimation();
 
     public static final TwinkleAnimation DISABLED_COLOR_1 = new TwinkleAnimation(0, 255, 255, 0, 0.5,
